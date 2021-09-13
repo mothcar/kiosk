a18e19ed064ece074797b79d84892539f4c5c560
Paul Hwang <mothcar@gmail.com>
2021-09-13 21:47:38 +0900

    start

---
+++ src/views/Duplicate.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container>
+    Page
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Page',
+    components: {
+    },
+
+    data() {
+      return{
+      }
+    }, // data
+
+    computed: {
+        // ...mapState('G_NAME', [])
+    }, // computed
+
+    mounted() {
+    }, // mounted
+
+    methods: {
+    }, // methods
+    
+  }
+</script>
